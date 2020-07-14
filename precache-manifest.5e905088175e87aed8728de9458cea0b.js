12bbe82b612ace04bc1a9e181f6c25d585078cc9
Florencia <flxrenciaemilia@gmail.com>
2020-07-14 00:56:39 -0500

    Updates

---
+++ precache-manifest.5e905088175e87aed8728de9458cea0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c84661ddf52f567be1085a56d60b714",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "cb78a1162e9e70e06485",
+    "url": "/Pomodoro-Clock/static/css/main.e5625d68.chunk.css"
+  },
+  {
+    "revision": "f6ede58c323970c3de36",
+    "url": "/Pomodoro-Clock/static/js/2.23283caa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.23283caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb78a1162e9e70e06485",
+    "url": "/Pomodoro-Clock/static/js/main.6c27c674.chunk.js"
+  },
+  {
+    "revision": "721be05b9d17e26ec034",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.7cfd1af6.js"
+  }
+]);
